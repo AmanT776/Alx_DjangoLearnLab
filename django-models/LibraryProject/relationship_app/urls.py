e96971f33d2ae82b10c59e68be0094186d6d4771
AmanT776 <amantes776@gmail.com>
2025-09-12 16:00:40 +0300

    feat: logout view created

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import reverse_lazy
 from .views import list_books, LibraryDetailView,SignupView,LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/',LoginView.as_view(),name='login'),
     path('signup/',SignupView.as_view(),name='signup'),
+    path('logout/',LogoutView.as_view(template_name="relationship_app/logout.html",reverse_lazy='login'),name='logout'),
     path('books/',list_books,name='books'),
     path('library/',LibraryDetailView.as_view()),
 ]
