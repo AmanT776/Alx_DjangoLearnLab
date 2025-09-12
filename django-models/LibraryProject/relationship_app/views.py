72dbc1224f0d54f72dbab3181040c422f5cb80c6
AmanT776 <amantes776@gmail.com>
2025-09-12 11:59:30 +0300

    feat: created list all books view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponse
+from .models import Book,Author
 # Create your views here.
+def list_all_books(request):
+    books = Book.objects.all()
+    titles = ", ".join(book.title + " " +  book.author.name for book in books)
+    return HttpResponse(titles)
