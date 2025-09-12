21867309bc6a2ade491f398f6608b718b0469def
AmanT776 <amantes776@gmail.com>
2025-09-12 14:20:17 +0300

    fix: fixed some type errors

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.http import HttpResponse
-from .models import Book,Author,Library
+from .models import Library,Book,Author
 # Create your views here.
 def list_all_books(request):
     books = Book.objects.all()
