0466c83204be854bd7f760e150b60bf1f50670cb
AmanT776 <amantes776@gmail.com>
2025-09-12 22:17:04 +0300

    fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, reverse_lazy
 from .views import admin_view, librarian_view, member_view, RegisterView, list_books, LibraryDetailView, BookCreateView, BookUpdateView, BookDeleteView
 from django.contrib.auth.views import LogoutView, LoginView
-
+["add_book/", "edit_book/", "delete_book"]
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('signup/', RegisterView.as_view(), name='signup'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('library/admin/', admin_view, name='admin_dashboard'),
     path('library/librarian/', librarian_view, name='librarian_dashboard'),
     path('library/member/', member_view, name='member_dashboard'),
-    path('books/add/', BookCreateView.as_view(), name='book_add'),
-    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book_edit'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('add_book/', BookCreateView.as_view(), name='book_add'),
+    path('edit_book/<int:pk>/', BookUpdateView.as_view(), name='book_edit'),
+    path('delete_book/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
 ]
