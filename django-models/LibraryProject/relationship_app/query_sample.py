5b98384e4ba8c8531d7913ab0e493b0f6ea355b0
AmanT776 <amantes776@gmail.com>
2025-09-12 11:05:03 +0300

    fix: fixed naming of functions in query_sample

--- django-models/LibraryProject/relationship_app/query_sample.py
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -1,18 +1,18 @@
 from . import models
 
-def books_by_author(author_name):
+def query_all_books_by_author(author_name):
     author = models.Author.objects.get(name=author_name)
     books = models.Book.objects.filter(author=author)
     for book in books:
         print(f"{book.id} {book.title}")
 
-def books_in_library(library_name):
+def list_all_books_in_library(library_name):
     library = models.Library.objects.get(name=library_name)
     books = models.Book.objects.filter(library=library)
     for book in books:
         print(book.title)
 
-def librarian_in_library(library_name):
+def retrieve_librarian_for_library(library_name):
     library = models.Library.objects.get(name=library_name)
     librarians = models.Librarian.objects.filter(library=library)
     for librarian in librarians:
