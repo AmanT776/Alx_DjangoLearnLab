596e6024fb1db7e85c407ccee6827ad8848ca4f2
AmanT776 <amantes776@gmail.com>
2025-09-12 11:00:27 +0300

    fix: fixed typo erros on query_sample

--- django-models/LibraryProject/relationship_app/query_sample.py
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -1,18 +1,18 @@
 from . import models
 
-def get_book_by_author(author_name):
+def books_by_author(author_name):
     author = models.Author.objects.get(name=author_name)
     books = models.Book.objects.filter(author=author)
     for book in books:
         print(f"{book.id} {book.title}")
 
-def get_books_in_library(library_name):
+def books_in_library(library_name):
     library = models.Library.objects.get(name=library_name)
     books = models.Book.objects.filter(library=library)
     for book in books:
         print(book.title)
 
-def get_librarian(library_name):
+def librarian_in_library(library_name):
     library = models.Library.objects.get(name=library_name)
     librarians = models.Librarian.objects.filter(library=library)
     for librarian in librarians:
