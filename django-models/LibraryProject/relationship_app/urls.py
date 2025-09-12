652a3c6af86807ad201d5b0a65f2036c47c78d20
AmanT776 <amantes776@gmail.com>
2025-09-12 16:02:56 +0300

    fix: fixed login view logic

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
-from django.urls import reverse_lazy
-from .views import list_books, LibraryDetailView,SignupView,LoginView
-from django.contrib.auth.views import LogoutView
+from django.urls import path, reverse_lazy
+from .views import list_books, LibraryDetailView,SignupView
+from django.contrib.auth.views import LogoutView,LoginView
 
 urlpatterns = [
-    path('login/',LoginView.as_view(),name='login'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('signup/',SignupView.as_view(),name='signup'),
-    path('logout/',LogoutView.as_view(template_name="relationship_app/logout.html",reverse_lazy='login'),name='logout'),
+    path('logout/',LogoutView.as_view(template_name="relationship_app/logout.html",),name='logout'),
     path('books/',list_books,name='books'),
     path('library/',LibraryDetailView.as_view()),
 ]
