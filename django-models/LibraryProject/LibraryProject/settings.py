e48b11cfc991fe59c4877c387a5332fde4a96916
AmanT776 <amantes776@gmail.com>
2025-09-12 14:56:31 +0300

    fixed tmeplate path

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['relationship_app.templates'],
+        'DIRS': ["relationship_app"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
