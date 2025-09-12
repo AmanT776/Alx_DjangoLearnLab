0eb2cf167faf68f3bc66c649f8601bb22328343b
AmanT776 <amantes776@gmail.com>
2025-09-12 14:49:33 +0300

    added publication_year attribute for book model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -9,6 +9,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    publication_year = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return self.title
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,4 +19,4 @@ class LibraryDetailView(ListView):
         libraries = context["libraries"]
         library = [library for library in libraries]
         content = {"libraries": library}
-        return render(self.request,"relationship_app/library_detail.html",content)
+        return render(self.request,"library_detail.html",content)
