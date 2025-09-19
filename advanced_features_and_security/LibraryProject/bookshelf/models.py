7846114c623c8d72b7535ff8f8d067fcc81312a3
AmanT776 <amantes776@gmail.com>
2025-09-19 16:16:31 +0300

    feat: created customusermanager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,23 +1,49 @@
 from django.db import models
 import datetime
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(default=datetime.date.today)
-    profile_photo = models.ImageField(upload_to='profile_photos/',blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True)
+
+    objects = CustomUserManager() 
 
     def __str__(self):
         return self.username
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
@@ -33,16 +59,17 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
 class Library(models.Model):
     name = models.CharField(max_length=30)
-    books = models.ManyToManyField(Book,related_name="books")
+    books = models.ManyToManyField(Book, related_name="books")
 
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=30)
-    library = models.OneToOneField(Library,on_delete=models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -53,8 +80,8 @@ class UserProfile(models.Model):
         ('LIBRARIAN', 'Librarian'),
         ('MEMBER', 'Member'),
     )
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='MEMBER')
 
     def __str__(self):
@@ -64,11 +91,11 @@ class UserProfile(models.Model):
         verbose_name = 'User Profile'
         verbose_name_plural = 'User Profiles'
 
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            UserProfile.objects.create(user=instance)
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
 
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
