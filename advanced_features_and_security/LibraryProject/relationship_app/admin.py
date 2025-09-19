15929fbfdd1258364a5fb7f4d8c959982ee0a792
AmanT776 <amantes776@gmail.com>
2025-09-19 16:05:52 +0300

    feat: integrated cutom user model into admin

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,6 +1,30 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+# relationship_app/admin.py
 
-# Register your models here.
-admin.site.register(User,UserAdmin)
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User, UserProfile
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (_('Additional Info'), {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (_('Additional Info'), {
+            'classes': ('wide',),
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    list_filter = ('role',)
