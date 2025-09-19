c51f84fc8d20ba2bfef3ddcd4e1518711833d8ba
AmanT776 <amantes776@gmail.com>
2025-09-19 16:55:03 +0300

    feat: created from_example form

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["relationship_app.templates"],
+        'DIRS': ["bookshelf.templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
