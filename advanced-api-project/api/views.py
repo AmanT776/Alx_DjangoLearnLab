18c0185e6547cdbb65243e525f7813abe60e8a85
AmanT776 <amantes776@gmail.com>
2025-10-04 10:15:19 +0300

    feat:-configured is authenticated persmission for views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,25 +3,25 @@ from rest_framework import generics
 from .models import Book,Author
 from .serializers import BookSerializer
 from rest_framework import serializers
-from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly,IsAuthenticated]
     authentication_classes = [BasicAuthentication]
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly,IsAuthenticated]
     authentication_classes = [BasicAuthentication]
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly,IsAuthenticated]
     authentication_classes = [BasicAuthentication]
 
     def perform_create(self, serializer):
@@ -32,7 +32,7 @@ class BookCreateView(generics.CreateAPIView):
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly,IsAuthenticated]
     authentication_classes = [BasicAuthentication]
 
     def perform_update(self, serializer):
@@ -42,5 +42,5 @@ class BookUpdateView(generics.UpdateAPIView):
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly,IsAuthenticated]
     authentication_classes = [BasicAuthentication]
