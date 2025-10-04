2b489520f30d86a98f7cf92f7de5e51552ea7e11
AmanT776 <amantes776@gmail.com>
2025-10-04 09:49:30 +0300

    feat:-customized book create view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book,Author
 from .serializers import BookSerializer
+from rest_framework import serializers
 # Create your views here.
 
 class BookListAPIView(generics.ListAPIView):
@@ -15,6 +16,11 @@ class BookDetailView(generics.RetrieveAPIView):
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        if Book.objects.filter(title=title).exists():
+            raise serializers.ValidationError({"message": "This book already exists."})
 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
