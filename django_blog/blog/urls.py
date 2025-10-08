846e258284247290ff8444d7e44b2beebb5313fb
AmanT776 <amantes776@gmail.com>
2025-10-08 09:48:08 +0300

    feat:-created profile section -style profile section

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterUser, LoginUser, home_view
+from .views import RegisterUser, LoginUser, home_view,profile_view
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('', home_view, name='home'),
+    path('profile',profile_view,name='profile')
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
@@ -7,12 +7,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ('email',)
+        fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name')
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
@@ -20,14 +22,27 @@ class UserRegistrationForm(UserCreationForm):
         self.fields['email'].widget.attrs.update({'class': 'form-control'})
         self.fields['password1'].widget.attrs.update({'class': 'form-control'})
         self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
         self.fields['username'].help_text = ''
         self.fields['password1'].help_text = ''
         self.fields['password2'].help_text = ''
+        
     def clean_password1(self):
         password = self.cleaned_data.get('password1')
         if len(password) < 3:
             raise ValidationError('password')
         return password
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class RegisterUser(CreateView):
     form_class = UserRegistrationForm
@@ -37,7 +52,27 @@ class RegisterUser(CreateView):
 class LoginUser(LoginView):
     template_name = 'blog/login.html'
     def get_success_url(self):
-        return reverse_lazy('home')
+        return reverse_lazy('profile')
 
 def home_view(request):
     return render(request,'blog/index.html')
+
+def profile_view(request):
+    if request.method == 'GET':
+        user = request.user
+        context = {"user": user}
+        return render(request,'blog/profile.html',context)
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated successfully!')
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    
+    context = {
+        'form': form,
+        'user': request.user
+    }
+    return render(request, 'blog/profile.html', context)
