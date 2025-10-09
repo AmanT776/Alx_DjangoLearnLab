b15afbf8b552ab75846693e2ed6e436c766dc852
AmanT776 <amantes776@gmail.com>
2025-10-09 21:33:20 +0300

    feat:-created tag model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -13,3 +13,7 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    post = models.ManyToManyField(Post,related_name="tags")
+    
