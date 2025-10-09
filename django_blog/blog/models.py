3b7e866fcdd01fd22014af0fabf68f25260c7230
AmanT776 <amantes776@gmail.com>
2025-10-09 22:09:11 +0300

    fix:-fixed errors on model attributes

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User,related_name="authors",on_delete=models.CASCADE)
+    author = models.ForeignKey(User,related_name="posts",on_delete=models.CASCADE)
     tags = TaggableManager()
 
     def __str__(self):
@@ -18,5 +18,5 @@ class Comment(models.Model):
     author = models.ForeignKey(User,related_name="comments",on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
